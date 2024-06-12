3489d355c96a52304bfd394fca0c58b7a9f08a9c
Carlassanchez24 <carlassanchez24@gmail.com>
2024-06-12 14:31:55 +0200

    Nabvar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-//import Navbar from '../src/components/navbar';
+import Navbar from './components/Navbar.jsx';
+
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar/>
+
   )
 }
